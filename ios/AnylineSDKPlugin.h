326ce8f2563b4344df2e283593cc9fb2f320fd25
redrose <business.mobile218@gmail.com>
2019-05-21 06:10:13 +0300

    fix ios build error

--- ios/AnylineSDKPlugin.h
+++ ios/AnylineSDKPlugin.h
@@ -1,6 +1,10 @@
 
 #import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else // Compatibility for RN version < 0.40
 #import "RCTBridgeModule.h"
+#endif
 
 @interface AnylineSDKPlugin : NSObject <RCTBridgeModule>
 
